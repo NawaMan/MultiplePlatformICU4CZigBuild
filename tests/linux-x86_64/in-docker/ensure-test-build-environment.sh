6945f60e994367cfe79a923cca420419cb007bd5
NawaMan <nawa@nawaman.net>
2025-08-09 00:29:19 -0400

    Build test for x86 and arm.

--- tests/linux-x86_64/in-docker/ensure-test-build-environment.sh
+++ tests/linux-x86_64/in-docker/ensure-test-build-environment.sh
@@ -6,20 +6,13 @@ source sh-sources/common-source.sh
 
 print_section "Installing dependencies"
 apt-get update
-apt-get install -y             \
-                               \
-                               \
-    build-essential            \
-    g++-multilib               \
-                               \
-    gcc-multilib               \
-    libfmt-dev                 \
-                               \
-                               \
-                               \
-                               \
-    tree                       \
-    unzip                      \
+apt-get install -y  \
+    build-essential \
+    g++-multilib    \
+    gcc-multilib    \
+    libfmt-dev      \
+    tree            \
+    unzip           \
     wget
 
 ./install-zig.sh
